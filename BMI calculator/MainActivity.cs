16495afd7720289fd8fb947cb6c540ee009a80c8
Johan Persson <joohan91@hotmail.com>
2017-11-15 11:04:51 +0100

    initial setup

---
+++ BMI calculator/MainActivity.cs
@@ -0,0 +1,89 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace BMI_calculator
+{
+    [Activity(Label = "BMI calculator", MainLauncher = true, Icon = "@drawable/startIconHappyScale")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            string imageName = "";
+            TextView textOutput;
+            textOutput = FindViewById<TextView>(Resource.Id.resultText);
+            Button calcButton = FindViewById<Button>(Resource.Id.calcBtn);
+            FindViewById<Button>(Resource.Id.calcBtn).Click += (o, e) =>
+            {
+                var weightNumber = (FindViewById<EditText>(Resource.Id.weightAdder));
+                var heightNumber = (FindViewById<EditText>(Resource.Id.heightAdder));
+
+
+                string weightInString = weightNumber.Text.ToString();
+                string heightInString = heightNumber.Text.ToString();
+
+                if (weightInString != "")
+                {
+                    if (heightInString != "")
+                    {
+                        decimal userWeight = decimal.Parse(weightNumber.Text.ToString());
+                        decimal userHeight = decimal.Parse(heightNumber.Text.ToString());
+                        decimal decVar = 0.01m;
+                        if (userHeight != 0)
+                        {
+                            decimal userHeightModified = (userHeight * decVar);
+                            decimal userHeightToCalc = userHeightModified * userHeightModified;
+                            decimal bmi = userWeight / userHeightToCalc;
+                            float bmiFloat = (float)bmi;
+                            string bmiOutput = bmiFloat.ToString();
+                            string weightText = "";
+                            if (bmiFloat < 18.5)
+                            {
+                                weightText = " you are underweight";
+                                imageName = "sadScale";
+                            }
+
+                            else if (bmiFloat >= 18.5 && bmiFloat <= 24.9)
+                            {
+                                weightText = " you have a normal weight";
+                                imageName = "happyScale";
+                            }
+
+                            else if (bmiFloat >= 25 && bmiFloat <= 30)
+                            {
+                                weightText = " you are overweight";
+                                imageName = "wonderingScale";
+                            }
+
+                            else
+                            {
+                                weightText = " you are obese";
+                                imageName = "angryScale";
+                            }
+
+                            //textOutput.Text = "Your BMI is " + bmiOutput + "," + weightText;
+                            string dialogText = "Your BMI is " + bmiOutput + "," + weightText;
+                            FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                            dialogSign signDialog = new dialogSign();
+
+                            signDialog.SendMessageToFragmentGreen(dialogText, imageName);
+
+                            signDialog.Show(transaction, "dialogFragment");
+
+                        }
+                    };
+                    //här under är den
+                };
+            };
+
+            }
+    }
+}
+
+
---
+++ BMI calculator/dialogSign.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BMI_calculator
+{
+
+    class dialogSign: DialogFragment
+    {
+        MainActivity mainObj = new MainActivity();
+        TextView textDialogSign;
+        ImageView imageDialog;
+        string message;
+        string imageName;
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            //base.OnCreateView(inflater, container, savedInstanceState);
+            //var view = inflater.Inflate(Resource.Layout.dialogSign, container, false);
+            //var rootView = inflater.Inflate(Resource.Layout.dialogSign, container, false);
+            //// I just assume the button is in HomePermainan
+            //textDialogSign = rootView.FindViewById<TextView>(Resource.Id.dialogResultText);
+            //return view;
+
+            View rootView = inflater.Inflate(Resource.Layout.dialogSign, container, false);
+            textDialogSign = rootView.FindViewById<TextView>(Resource.Id.dialogResultText);
+            imageDialog = rootView.FindViewById<ImageView>(Resource.Id.imageDisplay);
+
+            textDialogSign.Text = message;
+                     
+            if (imageName == "wonderingScale")
+            {
+                imageDialog.SetImageResource(Resource.Drawable.wonderingScale);
+            }
+            else if(imageName == "sadScale")
+            {
+                imageDialog.SetImageResource(Resource.Drawable.sadScale);
+            }
+            else if (imageName == "happyScale")
+            {
+                imageDialog.SetImageResource(Resource.Drawable.happyScale);
+            }
+            else if (imageName == "angryScale")
+            {
+                imageDialog.SetImageResource(Resource.Drawable.angryScale);
+            }
+
+            return rootView;
+        }
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialogAnimation;
+        }
+
+        public void SendMessageToFragmentGreen(string value, string imageTitle)
+        {
+            message = value;
+            imageName = imageTitle;
+        }
+
+        //public void GetMessage(string value)
+        //{
+        //    message = value;
+        //    textDialogSign.Text = message;
+        //}
+
+
+    }
+}
